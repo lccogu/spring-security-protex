2ff2755bc063d78009c0699713f3312e616d72bb
Jean Pierre-Conde <lccogu@gmail.com>
2019-10-21 18:47:39 -0500

    avance profe

--- src/main/java/pe/edu/protex/model/init/InitDB.java
+++ src/main/java/pe/edu/protex/model/init/InitDB.java
@@ -27,22 +27,32 @@ public class InitDB implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-/*		this.usuarioRepository.deleteAll();
+	this.usuarioRepository.deleteAll();
 		this.authorityRepository.deleteAll();
 		
 		Usuario juan = new Usuario();
 		juan.setUsername("juan");
 		juan.setPassword(passwordEncoder.encode("juan"));
+		juan.setApellidos("Flores");
+		juan.setNombres("Juan");
+		juan.setCargo("cargo");
+		
 		juan.setEnable(true);
 		
 		Usuario admin = new Usuario();
 		admin.setUsername("admin");
 		admin.setPassword(passwordEncoder.encode("admin"));
+		admin.setApellidos("Flores");
+		admin.setNombres("admin");
+		admin.setCargo("cargo");
 		admin.setEnable(true);
 		
         Usuario manager = new Usuario();
         manager.setUsername("manager");
         manager.setPassword(passwordEncoder.encode("manager"));
+        manager.setApellidos("Flores");
+        manager.setNombres("Juan");
+        manager.setCargo("cargo");
         manager.setEnable(true);
         
         juan.addAuthority("ROLE_USER");
@@ -53,6 +63,6 @@ public class InitDB implements CommandLineRunner{
         manager.addAuthority("ACCESS_REST1");
         
         List<Usuario> usuarios = Arrays.asList(juan, admin, manager);        
-        this.usuarioRepository.saveAll(usuarios);*/		
+        this.usuarioRepository.saveAll(usuarios);
 	}
 }
